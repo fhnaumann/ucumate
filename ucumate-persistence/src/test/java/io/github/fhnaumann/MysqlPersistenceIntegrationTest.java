e6ddd0da99997bc6a58e90b30b0bb79fe201b2c6
Felix <fhnaumann@t-online.de>
2026-01-03 12:57:48 +0100

    remove local debug code

--- ucumate-persistence/src/test/java/io/github/fhnaumann/MysqlPersistenceIntegrationTest.java
+++ ucumate-persistence/src/test/java/io/github/fhnaumann/MysqlPersistenceIntegrationTest.java
@@ -22,74 +22,42 @@ import java.sql.SQLException;
 @Testcontainers
 public class MysqlPersistenceIntegrationTest extends DBPersistenceIntegrationTestBase {
 
-    @Test
-    void debugDockerConnection() {
-        System.out.println("=== Environment ===");
-        System.out.println("DOCKER_HOST: " + System.getenv("DOCKER_HOST"));
-        System.out.println("User: " + System.getProperty("user.name"));
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8")
+            .withDatabaseName("ucumate_persistence")
+            .withUsername("test")
+            .withPassword("test");
 
-        System.out.println("\n=== Testcontainers Detection ===");
+    Connection connection;
+
+    @Override
+    protected void registerPersistenceProvider() {
         try {
-            DockerClientFactory factory = DockerClientFactory.instance();
-            System.out.println("Docker available: " + factory.isDockerAvailable());
-
-            // Try to get the client directly to see the error
-            DockerClient client = factory.client();
-            System.out.println("Client: " + client);
-            client.pingCmd().exec();
-            System.out.println("Ping successful!");
-        } catch (Exception e) {
-            System.out.println("ERROR:");
-            e.printStackTrace();
+            connection = DriverManager.getConnection(
+                    mysql.getJdbcUrl(),
+                    mysql.getUsername(),
+                    mysql.getPassword()
+            );
+            PersistenceRegistry.register("mysql", new MySQLPersistenceProvider(connection, null, null));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
-    protected void registerPersistenceProvider() {
-
-    }
-
-    @Override
     protected void clearDatabaseState() {
-
+        try {
+            connection = DriverManager.getConnection(
+                    mysql.getJdbcUrl(),
+                    mysql.getUsername(),
+                    mysql.getPassword()
+            );
+            try (var stmt = connection.createStatement()) {
+                stmt.execute("TRUNCATE TABLE ucumate_canonical");
+                stmt.execute("TRUNCATE TABLE ucumate_validate");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-//
-//    @Container
-//    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8")
-//            .withDatabaseName("ucumate_persistence")
-//            .withUsername("test")
-//            .withPassword("test");
-//
-//    Connection connection;
-//
-//    @Override
-//    protected void registerPersistenceProvider() {
-//        try {
-//            connection = DriverManager.getConnection(
-//                    mysql.getJdbcUrl(),
-//                    mysql.getUsername(),
-//                    mysql.getPassword()
-//            );
-//            PersistenceRegistry.register("mysql", new MySQLPersistenceProvider(connection, null, null));
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    @Override
-//    protected void clearDatabaseState() {
-//        try {
-//            connection = DriverManager.getConnection(
-//                    mysql.getJdbcUrl(),
-//                    mysql.getUsername(),
-//                    mysql.getPassword()
-//            );
-//            try (var stmt = connection.createStatement()) {
-//                stmt.execute("TRUNCATE TABLE ucumate_canonical");
-//                stmt.execute("TRUNCATE TABLE ucumate_validate");
-//            }
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
